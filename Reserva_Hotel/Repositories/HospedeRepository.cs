5aa41c57ae034601d715d5c14fab15707ee4c81d
NataliaMZ-IT <zamperlinin@gmail.com>
2026-05-19 00:23:12 -0300

    Fix: abertura de conexao com banco e adicao do bloco try-catch

--- Reserva_Hotel/Repositories/HospedeRepository.cs
+++ Reserva_Hotel/Repositories/HospedeRepository.cs
@@ -1,55 +1,94 @@
 ﻿using Dapper;
 using MySql.Data.MySqlClient;
 using Reserva_Hotel.Models;
+using System;
 using System.Collections.Generic;
 
 namespace Reserva_Hotel.Repositories
 {
     public class HospedeRepository : IRepository<Hospede>
     {
-        private readonly string conexao = "Server=localhost;Database=Hotel;Uid=root;Pwd=root;";
+        private readonly string _connectionString = "Server=localhost;Database=Hotel;Uid=root;Pwd=root;";
 
         public void Inserir(Hospede hospede)
         {
-            using (MySqlConnection banco = new MySqlConnection(conexao))
+            try
             {
-                string sql = @"INSERT INTO hospedes (nome, sobrenome, cpf, dataNascimento, email, telefone) 
-                               VALUES (@Nome, @Sobrenome, @Cpf, @DataNascimento, @Email, @Telefone);";
+                using (MySqlConnection conn = new MySqlConnection(_connectionString))
+                {
+                    conn.Open();
 
-                banco.Execute(sql, hospede);
+                    string sql = @"INSERT INTO hospedes (nome, sobrenome, cpf, dataNascimento, email, telefone) 
+                                   VALUES (@Nome, @Sobrenome, @Cpf, @DataNascimento, @Email, @Telefone);";
+
+                    conn.Execute(sql, hospede);
+                }
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception($"Erro com o banco de dados: {ex.Message}");
             }
         }
 
         public IEnumerable<Hospede> ObterTodos()
         {
-            using (MySqlConnection banco = new MySqlConnection(conexao))
+            try
             {
-                string sql = "SELECT * FROM hospedes;";
+                using (MySqlConnection conn = new MySqlConnection(_connectionString))
+                {
+                    conn.Open();
 
-                return banco.Query<Hospede>(sql);
+                    string sql = "SELECT * FROM hospedes;";
+
+                    return conn.Query<Hospede>(sql);
+                }
             }
+            catch (MySqlException ex) 
+            {
+                throw new Exception($"Erro com o banco de dados: {ex.Message}");
+            }
+
         }
 
         public void Editar(Hospede hospede)
         {
-            using (MySqlConnection banco = new MySqlConnection(conexao))
+            try
             {
-                string sql = @"UPDATE hospedes
+
+                using (MySqlConnection conn = new MySqlConnection(_connectionString))
+                {
+                    conn.Open();
+
+                    string sql = @"UPDATE hospedes
                                SET nome = @Nome, sobrenome = @Sobrenome, cpf = @Cpf, dataNascimento = @DataNascimento
                                    email = @Email, telefone = @Telefone
                                WHERE id = @Id;";
 
-                banco.Execute(sql, hospede);
+                    conn.Execute(sql, hospede);
+                }
+            }
+            catch (MySqlException ex) 
+            {
+                throw new Exception($"Erro com o banco de dados: {ex.Message}");
             }
         }
 
         public void Deletar(int id)
         {
-            using (MySqlConnection banco = new MySqlConnection(conexao))
+            try
             {
-                string sql = "DELETE FROM hospedes WHERE id = @Id;";
+                using (MySqlConnection conn = new MySqlConnection(_connectionString))
+                {
+                    conn.Open();
 
-                banco.Execute(sql, new { Id = id });
+                    string sql = "DELETE FROM hospedes WHERE id = @Id;";
+
+                    conn.Execute(sql, new { Id = id });
+                }
+            }
+            catch (MySqlException ex) 
+            {
+                throw new Exception($"Erro com o banco de dados: {ex.Message}");
             }
         }
     }
