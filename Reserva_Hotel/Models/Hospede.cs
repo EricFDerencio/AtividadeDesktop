2163e676a029a498201f65841b4aefda34b60b6f
NataliaMZ-IT <zamperlinin@gmail.com>
2026-05-18 21:15:31 -0300

    Add: classe hospede e repository dela

---
+++ Reserva_Hotel/Models/Hospede.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Reserva_Hotel.Models
+{
+    public class Hospede
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public string PrintMessage()
+        {
+            return $"Hospede {this.Id}" +
+                $"\nNome: {this.Nome} {this.Sobrenome}" +
+                $"\nCPF: {this.Cpf}" +
+                $"\nData de Nascimento: {this.DataNascimento}" +
+                $"\nEmail: {this.Email}" +
+                $"\nTelefone: {this.Telefone}";
+        }
+
+    }
+}
---
+++ Reserva_Hotel/Repositories/HospedeRepository.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using Reserva_Hotel.Models;
+using System.Collections.Generic;
+
+namespace Reserva_Hotel.Repositories
+{
+    public class HospedeRepository : IRepository<Hospede>
+    {
+        private readonly string conexao = "Server=localhost;Database=Hotel;Uid=root;Pwd=root;";
+
+        public void Inserir(Hospede hospede)
+        {
+            using (MySqlConnection banco = new MySqlConnection(conexao))
+            {
+                string sql = @"INSERT INTO hospedes (nome, sobrenome, cpf, dataNascimento, email, telefone) 
+                               VALUES (@Nome, @Sobrenome, @Cpf, @DataNascimento, @Email, @Telefone);";
+
+                banco.Execute(sql, hospede);
+            }
+        }
+
+        public IEnumerable<Hospede> ObterTodos()
+        {
+            using (MySqlConnection banco = new MySqlConnection(conexao))
+            {
+                string sql = "SELECT * FROM hospedes;";
+
+                return banco.Query<Hospede>(sql);
+            }
+        }
+
+        public void Editar(Hospede hospede)
+        {
+            using (MySqlConnection banco = new MySqlConnection(conexao))
+            {
+                string sql = @"UPDATE hospedes
+                               SET nome = @Nome, sobrenome = @Sobrenome, cpf = @Cpf, dataNascimento = @DataNascimento
+                                   email = @Email, telefone = @Telefone
+                               WHERE id = @Id;";
+
+                banco.Execute(sql, hospede);
+            }
+        }
+
+        public void Deletar(int id)
+        {
+            using (MySqlConnection banco = new MySqlConnection(conexao))
+            {
+                string sql = "DELETE FROM hospedes WHERE id = @Id;";
+
+                banco.Execute(sql, new { Id = id });
+            }
+        }
+    }
+}
