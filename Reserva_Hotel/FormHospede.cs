50133b6eb9534eac9ed286e976c450d9d32a0abd
NataliaMZ-IT <zamperlinin@gmail.com>
2026-05-19 15:43:01 -0300

    Add: codigo para tela principal de hospede

--- Reserva_Hotel/FormHospede.cs
+++ Reserva_Hotel/FormHospede.cs
@@ -1,12 +1,6 @@
 ﻿using Reserva_Hotel.Repositories;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Reserva_Hotel
@@ -38,10 +32,10 @@ namespace Reserva_Hotel
         {
             try
             {
-                var alunos = _hospedeRepository.ObterTodos();
-                gridHospedes.DataSource = alunos;
+                var hospedes = _hospedeRepository.ObterTodos();
+                gridHospedes.DataSource = hospedes;
                 lblTotalHospedes.Text =
-                    $"Total: {alunos.ToList().Count} alunos";
+                    $"Total: {hospedes.ToList().Count} cadastros";
             }
             catch (Exception ex)
             {
@@ -51,27 +45,104 @@ namespace Reserva_Hotel
 
         private void btnInserir_Click(object sender, EventArgs e)
         {
+            try
+            {
+                FormEditarHospede formNovoCadastro = new FormEditarHospede(null, _hospedeRepository);
 
+                if (formNovoCadastro.ShowDialog() == DialogResult.OK)
+                {
+                    CarregarGrid();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao inserir cadastro: {ex.Message}", "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void btnAlterar_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (gridHospedes.SelectedRows.Count != 1)
+                {
+                    MessageBox.Show("Seleciona um hospede para editar seus dados");
+                    return;
+                }
 
+                int id = (int)gridHospedes.SelectedRows[0].Cells["id"].Value;
+                var hospede = _hospedeRepository.ObterUm(id);
+
+                if (hospede != null)
+                {
+                    FormEditarHospede formEditarHospede = new FormEditarHospede(hospede, _hospedeRepository);
+                    
+                    if (formEditarHospede.ShowDialog() == DialogResult.OK)
+                    {
+                        CarregarGrid();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao editar cadastro: {ex.Message}", "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void btnDeletar_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (gridHospedes.SelectedRows.Count != 1)
+                {
+                    MessageBox.Show("Seleciona um hospede para apagar seus dados");
+                    return;
+                }
 
+                int id = (int)gridHospedes.SelectedRows[0].Cells["Id"].Value;
+                string nome = gridHospedes.SelectedRows[0].Cells["Nome"].Value.ToString() + 
+                    " " + 
+                    gridHospedes.SelectedRows[0].Cells["Sobrenome"].Value.ToString();
+
+                DialogResult resultado = MessageBox.Show(
+                    $"Deseja mesmo deletar '{nome}' e seus dados?",
+                    "Confirmação",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (resultado == DialogResult.Yes)
+                {
+                    _hospedeRepository.Deletar(id);
+                    MessageBox.Show("Deletado com sucesso!", "Sucesso",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    
+                    CarregarGrid();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao deletar cadastro: {ex.Message}", "Erro",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void btnVoltar_Click(object sender, EventArgs e)
         {
-            Close();
+            this.Close();
         }
 
         private void btnBuscar_Click(object sender, EventArgs e)
         {
-
+            if (string.IsNullOrWhiteSpace(txtBxBuscaNome.Text) && 
+                string.IsNullOrWhiteSpace(txtBxBuscaSobrenome.Text))
+                CarregarGrid();
+            else
+            {
+                var hospedes = _hospedeRepository.ObterPorNome(txtBxBuscaNome.Text, txtBxBuscaSobrenome.Text).ToList();
+                gridHospedes.DataSource = hospedes;
+                lblTotalHospedes.Text = $"Total: {hospedes.Count} cadastros";
+            }
         }
     }
 }
